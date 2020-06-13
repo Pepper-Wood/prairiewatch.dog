e8ac5124559b82bd7e5c2988b0ad5039415e56b4
Kathryn DiPippo <dipippo.k@gmail.com>
2020-06-12 20:54:50 -0400

    Set up Angular site with a home component and an entry component with URL routing

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

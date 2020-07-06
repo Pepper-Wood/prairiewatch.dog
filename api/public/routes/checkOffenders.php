956688ec197fe117d29bfb3c73483bce29e14e54
Kathryn DiPippo <dipippo.k@gmail.com>
2020-07-05 23:30:00 -0400

    Day 23 | Implemented checkOffenders.php

--- api/public/routes/checkOffenders.php
+++ api/public/routes/checkOffenders.php
@@ -4,47 +4,46 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Response;
+use Symfony\Component\Yaml\Yaml;
 
 $app->get('/v0/offenders', function (Request $request, ResponseInterface $response, $args): ResponseInterface {
-    $params = getQueryParams();
-    if ($params["twitter"]) {
-        return True;
+    $result = [];
+    $params = $request->getQueryParams();
+    if (array_key_exists("twitter", $params)) {
+        $url_text = file_get_contents("https://raw.githubusercontent.com/Pepper-Wood/prairiewatch.dog/master/database/listing/twitter.yml");
+        $twitter_handles = Yaml::parse($url_text);
+        $handles = explode(",", $params["twitter"]);
+        foreach ($handles as $handle) {
+            if (array_key_exists($handle, $twitter_handles)) {
+                $url_offender = "https://raw.githubusercontent.com/Pepper-Wood/prairiewatch.dog/master/database/offenders/" . $twitter_handles[$handle] . ".yml";
+                $url_text = file_get_contents($url_offender);
+                $result[] = Yaml::parse($url_text);
+            }
+        }
     }
-    if ($params["url"]) {
-        return True;
+    if (array_key_exists("url", $params)) {
+        $url_text = file_get_contents("https://raw.githubusercontent.com/Pepper-Wood/prairiewatch.dog/master/database/listing/websites.yml");
+        $websites_list = Yaml::parse($url_text);
+        $url_param = $params["url"];
+        $url_matches = array_filter($websites_list, function($url_key) use($url_param) {
+            // return true if url_key is a substring of the url_param
+            // i.e., param might be https://lularoe.com/products/amelia
+            //       key would be lularoe.com
+            return (strpos($url_param, $url_key) !== false);
+        }, ARRAY_FILTER_USE_KEY);
+        var_dump($url_matches);
+        foreach ($url_matches as $url_match) {
+            var_dump($url_match);
+            $url_offender = "https://raw.githubusercontent.com/Pepper-Wood/prairiewatch.dog/master/database/offenders/" . $websites_list[$url_match] . ".yml";
+            $url_text = file_get_contents($url_offender);
+            $result[] = Yaml::parse($url_text);
+        }
     }
-    if ($args["site"] == "twitter") {
-        $example = [
-            ["lularoe" => "0680b520-0252-440f-953c-fcec27740a45"],
-            ["noahbradley" => "4ed9aad9-a070-4fc4-9f9c-cfd87756e72d"]
-        ];
-        $example = [
-            "offenders" => $example
-        ];
-        $response->getBody()->write(json_encode($example));
-        return $response->withStatus(200)
-            ->withHeader('Content-Type', 'application/json')
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    } else if ($args["site"] == "websites") {
-        $example = [
-            "lularoe.com" => "0680b520-0252-440f-953c-fcec27740a45",
-            "noahbradley.com" => "4ed9aad9-a070-4fc4-9f9c-cfd87756e72d"
-        ];
-        $example = [
-            "offenders" => $example
-        ];
-        $response->getBody()->write(json_encode($example));
-        return $response->withStatus(200)
-            ->withHeader('Content-Type', 'application/json')
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    }
-    $error = ["error" => "Site argument not recognized."];
-    $response->getBody()->write(json_encode($error));
-    return $response->withStatus(404)
+    $example = [
+        "offenders" => $result
+    ];
+    $response->getBody()->write(json_encode($example));
+    return $response->withStatus(200)
         ->withHeader('Content-Type', 'application/json')
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
--- api/public/routes/getOffender.php
+++ api/public/routes/getOffender.php
@@ -6,11 +6,16 @@ use Slim\Factory\AppFactory;
 use Slim\Psr7\Response;
 use Symfony\Component\Yaml\Yaml;
 
-$app->get('/v0/offender/{uuid}', function (Request $request, ResponseInterface $response, $args): ResponseInterface {
+$app->get('/v0/offenders/{uuid}', function (Request $request, ResponseInterface $response, $args): ResponseInterface {
     $url = "https://raw.githubusercontent.com/Pepper-Wood/prairiewatch.dog/master/database/offenders/" . $args["uuid"] . ".yml";
     $headers = @get_headers($url);
     if (!$headers || !strpos($headers[0],'200')) {
-        $error = ["error" => "UUID for offender not found."];
+        $error = [
+            "error" => [
+                "status" => 404,
+                "message" => "Offender UUID for offender not found."
+            ]
+        ];
         $response->getBody()->write(json_encode($error));
         return $response->withStatus(404)
             ->withHeader('Content-Type', 'application/json')
@@ -20,10 +25,7 @@ $app->get('/v0/offender/{uuid}', function (Request $request, ResponseInterface $
     }
     $url_text = file_get_contents($url);
     $offender = Yaml::parse($url_text);
-    $example = [
-        "offender" => $offender
-    ];
-    $response->getBody()->write(json_encode($example));
+    $response->getBody()->write(json_encode($offender));
     return $response->withStatus(200)
         ->withHeader('Content-Type', 'application/json')
         ->withHeader('Access-Control-Allow-Origin', '*')

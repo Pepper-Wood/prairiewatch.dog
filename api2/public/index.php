a258834872092a23f3024341df2e69c6a1673402
Kathryn DiPippo <kathryn.dipippo@acquia.com>
2020-09-19 20:00:39 -0400

    Re-removed api-related folders

--- api2/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../Controller/ApiController.php';
-require __DIR__.'/../Controller/CheckOffendersController.php';
-require __DIR__.'/../Controller/OffendersController.php';
-require __DIR__.'/../Routes.php';
